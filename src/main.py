da04d4c075624e890005c7bf1aba5e5ea52359d0
VirtualWolf <virtualwolf@virtualwolf.org>
2022-12-18 19:54:18 +1100

    Updated the status LED changing function to use events instead of callbacks.

--- src/main.py
+++ src/main.py
@@ -18,16 +18,29 @@ def set_time(timer = None):
         logger.log('Failed to contact NTP server at %s' % config['ntp_server'])
         reset()
 
-async def wifi_handler(state):
+def set_connection_status(state):
     led = Pin(13, Pin.OUT)
 
     if state:
         logger.log('Wifi and MQTT broker are up')
-        led.value(1)
+        led.value(0)
     else:
         logger.log('Wifi or MQTT broker is down')
-        led.value(0)
-    await asyncio.sleep(1)
+        led.value(1)
+
+async def up(client):
+    while True:
+        await client.up.wait()
+        client.up.clear()
+        set_connection_status(True)
+
+async def down(client):
+    while True:
+        await client.down.wait()  # Pause until connectivity changes
+        client.down.clear()
+        set_connection_status(False)
+
+set_connection_status(False)
 
 async def main(client):
     try:
@@ -43,13 +56,12 @@ async def main(client):
     set_time_timer = Timer(0)
     set_time_timer.init(mode=Timer.PERIODIC, period=86400000, callback=set_time)
 
-    for coroutine in (updater.subscribe, updater.messages):
+    for coroutine in (up, down, updater.subscribe, updater.messages):
         asyncio.create_task(coroutine(client))
 
     while True:
         await sensor.read_sensor(client)
 
-mqtt_config['wifi_coro'] = wifi_handler
 mqtt_config['clean'] = False
 mqtt_config['queue_len'] = 10
 
