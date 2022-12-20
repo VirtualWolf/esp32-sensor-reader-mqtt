cbd16706f36463a263240c0f8d2be717f91bd803
VirtualWolf <virtualwolf@virtualwolf.org>
2022-12-20 21:55:25 +1100

    Added ability to remotely update config.json via a message to the MQTT updates topic.

--- src/updater.py
+++ src/updater.py
@@ -1,37 +1,66 @@
 import gc
+import ujson
 from machine import reset
-from config import config
 from logger import log
 
 async def messages(client):
     async for topic, msg, retained in client.queue:
-        if msg.decode() == 'update':
-            log('Triggering an update check!')
+        payload = ujson.loads(msg.decode())
 
-            gc.collect()
+        if 'update_code' in payload and payload['update_code'] == True:
+            start_code_update()
 
-            import senko
+        if 'config' in payload:
+            start_config_update(payload['config'])
 
-            files = [
-                'senko/__init__.py',
-                'senko/senko.py',
-                'mqtt_as.py',
-                'config.py',
-                'logger.py',
-                'main.py',
-                'sensor.py',
-                'updater.py'
-            ]
 
-            OTA = senko.Senko(
-                user='VirtualWolf',
-                repo='esp32-sensor-reader-mqtt',
-                branch='main',
-                working_dir='src',
-                files=files)
 
-            if OTA.update():
-                log('Updated to the latest version! Rebooting...')
-                reset()
-            else:
-                log('Already the latest version!')
+def start_config_update(incoming_config):
+    log('Triggered a configuration update!')
+
+    with open('config.json', 'r') as file:
+        current_config = ujson.load(file)
+
+    log('Current configuration:')
+    log(current_config)
+
+    current_config.update(incoming_config)
+
+    log('Updated configuration:')
+    log(current_config)
+
+    with open('config.json', 'w') as file:
+        ujson.dump(current_config, file)
+
+    reset()
+
+def start_code_update():
+    log('Triggered an update check!')
+
+    gc.collect()
+
+    import senko
+
+    files = [
+        'senko/__init__.py',
+        'senko/senko.py',
+        'mqtt_as.py',
+        'config.py',
+        'logger.py',
+        'main.py',
+        'sensor.py',
+        'updater.py'
+    ]
+
+    OTA = senko.Senko(
+        user='VirtualWolf',
+        repo='esp32-sensor-reader-mqtt',
+        branch='main',
+        working_dir='src',
+        files=files)
+
+    if OTA.update():
+        log('Updated to the latest version! Rebooting...')
+        reset()
+    else:
+        log('Already the latest version!')
